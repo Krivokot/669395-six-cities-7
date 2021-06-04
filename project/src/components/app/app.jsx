76db8edf554ebadb3c820c5f7af1c6f5ff9606fd
Krivokot <37597505+Krivokot@users.noreply.github.com>
2021-06-04 14:30:46 +0300

    Добавляет отрисовку компонента

--- project/src/components/app/app.jsx
+++ project/src/components/app/app.jsx
@@ -1,7 +1,23 @@
 import React from 'react';
+import Main from '../main/main';
+import PropTypes from 'prop-types';
 
-function App() {
-  return <p>Hello, world!</p>;
+function App(props) {
+  const {citiesCardsList, citiesCardsListLength} = props;
+
+  return (
+    <Main citiesCardsListLength = {citiesCardsListLength} citiesCardsList = {citiesCardsList}/>
+  );
 }
+
+App.propTypes = {
+  citiesCardsListLength: PropTypes.number.isRequired,
+  citiesCardsList: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number.isRequired,
+      title: PropTypes.string,
+    }),
+  ).isRequired,
+};
 
 export default App;
--- project/src/index.js
+++ project/src/index.js
@@ -2,8 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/app/app';
 
+const cardsArray = [1, 2, 3, 4, 5];
+
+const Setting = {
+  CARDS_COUNT: cardsArray,
+  PLACES_COUNT: cardsArray.length,
+};
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App
+      citiesCardsListLength = {Setting.PLACES_COUNT}
+      citiesCardsList = {Setting.CARDS_COUNT}
+    />
   </React.StrictMode>,
-  document.getElementById('root'));
+  document.getElementById('root'),
+);
