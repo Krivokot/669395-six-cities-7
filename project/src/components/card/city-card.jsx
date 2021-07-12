b81069c13eb0adbc3e4cfa95fc5c07a187f6f510
Krivokot <37597505+Krivokot@users.noreply.github.com>
2021-07-13 00:36:03 +0300

    Финальные правки

--- project/src/components/card/city-card.jsx
+++ project/src/components/card/city-card.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import {AppRoute, CardTypes} from '../../const';
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 
 function CityCard(props) {
   const {offer, cardType, titleEnter} = props;
